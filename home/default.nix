563e6230fb0f6f87eea611aa6d2887755178bb0e
gutehall <mathias@dm3.io>
2024-05-21 10:59:46 +0200

    Added more dotfiles

--- home/default.nix
+++ home/default.nix
@@ -15,7 +15,11 @@
     fonts.fontconfig.enable = true;
 
     home.file."./dotfiles/.vimrc".source = ./Users/mathias/.vimrc;
-    home.file."/dotfiles/.zshrc".source = ./Users/mathias/.zshrc;
+    home.file."./dotfiles/.zshrc".source = ./Users/mathias/.zshrc;
+    home.file."./dotfiles/Default.json".source = ./Users/mathias/Documents/Default.json;
+    home.file."./dotfiles/Hotkey.json".source = ./Users/mathias/Documents/Hotkey.json;
+    home.file."./dotfiles/com.googlecode.iterm2.plist".source = ./Users/mathias/Documents/com.googlecode.iterm2.plist;
+    home.file."./dotfiles/bullet-train.zsh-theme".source = ./Users/mathias/.oh-my-zsh/themes/bullet-train.zsh-theme;
 
     home.sessionVariables = {
       EDITOR = "vim";
